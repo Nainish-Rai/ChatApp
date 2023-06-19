f1d7827f981bff2ce5c599e2a93e582bb8fb5912
Nainish Rai <nainishraisjp@gmail.com>
2023-06-19 15:12:53 +0530

    client initial commit

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const {Server} = require('socket.io')
 
 app.use(cors()); // Add cors middleware
 
 const server = http.createServer(app);
+
+const io = new Server (server,{
+  cors : {
+    origin:"http://localhost:4000",
+    methods:['GET','POST']
+  }
+})
+
+io.on('connection',(socket)=>{
+  console.log('user connected on', socket.id)
+
+})
 app.get('/', (req, res) => {
     res.send('Hello world');
   });
